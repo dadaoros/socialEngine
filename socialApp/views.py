f174745208bb91befff28bbe226265e6c4044a8e
Miguel Gutierrez <ryuuzakiupldr@gmail.com>
2014-07-11 13:49:06 -0500

    template folder moved to socialEngine

--- socialApp/views.py
+++ socialApp/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
 
 def register(request):
-    return render_to_response('html/register.html')
+    return render_to_response('register.html')
 
 def login(request):
-    return render_to_response('html/login.html')
+    return render_to_response('login.html')
 
-def follows(request).
-    return render_to_response('html/follows.html')
+def follows(request):
+    return render_to_response('follows.html')
