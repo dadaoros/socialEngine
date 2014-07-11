0ec14e853a36ce2cffe9fbae0ffefcf9fc198aa0
Miguel Gutierrez <ryuuzakiupldr@gmail.com>
2014-07-11 14:00:37 -0500

    url for home

--- socialEngine/urls.py
+++ socialEngine/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    #url(r'^$', 'socialEngine.views.home', name='home'),
+    url(r'^$', 'socialApp.views.login'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^register/', 'socialApp.views.register'),
     url(r'^login/', 'socialApp.views.login'),
