46154691c9d0962f6b61f2320e16dd1cf6b9b8db
Miguel Gutierrez <ryuuzakiupldr@gmail.com>
2014-07-22 12:36:58 -0500

    follows model fixed

--- socialApp/models.py
+++ socialApp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Profile(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=30)
-    followers = models.ForeignKey('self',related_name='Followers', blank=True, null=True, on_delete=models.SET_NULL)
-    followings=models.ForeignKey('self',related_name='Followings', blank=True, null=True, on_delete=models.SET_NULL)
+    followers = models.ManyToManyField('self',related_name='Followers', blank=True, null=True, on_delete=models.SET_NULL,symmetrical=False)
+    followings=models.ManyToManyField('self',related_name='Followings', blank=True, null=True, on_delete=models.SET_NULL,symmetrical=False)
     def __unicode__(self):
         return self.email
 
