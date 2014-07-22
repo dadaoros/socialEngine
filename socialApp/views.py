2b60e53e0401a4fdc1a777488494b2f61fa955bb
Miguel Gutierrez <ryuuzakiupldr@gmail.com>
2014-07-22 13:02:39 -0500

    updated login view

--- socialApp/views.py
+++ socialApp/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
 
-# Create your views here.
 
+def home(request):
+    return render_to_response('home.html')
+    
 def register(request):
     return render_to_response('register.html')
 
 def login(request):
-    return render_to_response('login.html')
-
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
+    
+def error_login(request):
+    return render_to_response('error_login.html')
+    
 def follows(request):
     return render_to_response('follows.html')
