e31ea1a9f79b330a70e3bb82a6b444d56b39fe7f
Miguel Gutierrez <ryuuzakiupldr@gmail.com>
2014-07-24 15:00:06 -0500

    view for register_user

--- socialApp/views.py
+++ socialApp/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
+from django.contrib.auth.forms import UserCreationForm
 
 @login_required(login_url='/login/')
 def home(request):
@@ -40,3 +41,14 @@ def error_login(request):
     
 def follows(request):
     return render_to_response('follows.html')
+
+def register_user(request):
+    if request.method == POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/register_success')
+    args = {}
+    args.update(csrf(request))
+    
+    args['form'] = UserCreationForm()
+    return render_to_response('register.html', args)
